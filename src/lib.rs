90b44059ca9a7f9b69a52a97d5b735a4fab1c862
BuildTools <DeoTimeTheGithubUser@users.noreply.github.com>
2023-04-21 16:30:46 -0500

    bruh

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,54 @@
 #![feature(generic_const_exprs)]
-#![feature(trait_alias)]
 
 
-pub trait Check<const EXPRESSION: bool> {}
+pub enum Check<const EXPRESSION: bool> {}
 
-pub trait Passed = Check<true>;
-pub trait Failed = Check<false>;
-
-pub enum Condition<const CONDITION: bool> {}
-impl<const CONDITION: bool> Check<CONDITION> for Condition<CONDITION> {}
+pub trait Passed {}
+pub trait Failed {}
 
 pub enum Equals<const A: i32, const B: i32> {}
-impl<const A: i32, const B: i32> Check<{ A == B }> for Equals<A, B> {}
+pub enum Not<const EXPRESSION: bool> {}
+impl<const A: i32, const B: i32> Passed for Equals<A, B>
+    where Check<{ A == B }>: Passed {
+}
+
+impl<const A: i32, const B: i32> Failed for Equals<A, B>
+    where Check<{ A == B }>: Failed {
+}
 
 struct Test<const T: i32>(i32)
     where Equals<T, 2>: Passed;
 
+// macro_rules! const_category {
+//     (numbers) => (i8 i16 i32 i64)
+// }
+//
 // #[macro_export]
 // macro_rules! define_check {
-//     ($name:ident for $cc:ident, ($($param:ident)+) => $check:expr) => {
+//     ($name:ident for $($const_type:ty)|+, |$($param:ident),+| $check:expr) => {
+//         macro_rules! create_check_for_ctype {
+//             ($ctype:ty) => {
+//                 pub enum $name<const $($param)*: $ctype> {}
+//             }
+//         }
+//
 //         $(
-//         pub trait $name = Check<$($param),*>;
+//             create_check_for_ctype!($const_type);
 //         )*
 //
-//     }
+//
+//     };
+//
+//     // (@internal $name:ident, $($ctype:ty) +, $constants:tt, $params:tt, $check:expr) => {
+//     //
+//     // }
 // }
+
+// define_check!(Equals for i32, (A, B) => A == B);
 
 
 
 #[test]
 fn test_condition() {
-
+    define_check!(Hello for i32 | bool, |A| true);
 }
