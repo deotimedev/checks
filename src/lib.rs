0e213785d55b8444ff8674bbb295aedc47b05119
deotime <89555032+DeoTimeTheGithubUser@users.noreply.github.com>
2023-04-22 18:20:39 -0500

    Much more checks

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,9 @@
 
 pub enum Check<const E: bool> {}
 
+
 pub trait Conclusion<const R: bool> {}
 
-// These can be represented by a trait alias to `Conclusion` when supported.
 pub trait Passed {}
 pub trait Failed {}
 
@@ -13,24 +13,31 @@ impl<T: Conclusion<true>> Passed for T {}
 impl<T: Conclusion<false>> Failed for T {}
 
 // Theoretically, all custom checks could just be represented as a
-// type alias of `Check`, however currently it complains that it would
-// be an unconstrained bound.
+// type alias of `Check`, however currently the compiler complains
+// that it would be an unconstrained bound
 macro_rules! check_module {
-    ($($mod:ident)* => $($(#[doc = $doc:expr])?$name:ident$((passes: $($($pass:expr),*);*))?: |$($param:ident),+$(,)?| $check:expr)*) => {
+    ($($mod:ident)* => $($(#[doc = $doc:expr])?$name:ident$((passes: $($($pass:tt);*),* fails: $($($fail:expr);*),*))?: |$($param:ident),+$(,)?| $check:expr)*) => {
         macro_rules! apply_module {
             ($m:ident) => {
 
                 pub mod $m {
                     $(
+                        // TODO: Find out why doc tests don't work here even when they do normally
+
                         $(#[doc = $doc])*
                         /// ```no_run
                         #[doc = concat!("use checks::{ Passed, ", stringify!($m), "::* };")]
-                        #[doc = concat!("struct Test<", $(concat!("const ", stringify!($param), ": ", stringify!($m), ", ")),*, ">")]
+                        #[doc = concat!("struct ", stringify!($name), "Test<", $(concat!("const ", stringify!($param), ": ", stringify!($m), ", ")),*, ">")]
                         #[doc = concat!("\twhere ", stringify!($name), "<", stringify!($($param),*), ">: Passed;")]
                         ///
-                        /// // All compile
                         $($(
-                            #[doc = concat!("let works = Test::<", stringify!($($pass),*), ">;")]
+                            #[doc = concat!("let works = ", stringify!($name),"Test::<", stringify!($($pass),*), ">; // Success!")]
+                        )*)*
+                        /// ```
+
+                        /// ```compile_fail
+                        $($(
+                            #[doc = concat!("let doesnt_work = ", stringify!($name),"Test::<", stringify!($($fail),*), ">; // Compile error!")]
                         )*)*
                         /// ```
                         pub enum $name<$(const $param: $m,)*> {}
@@ -45,75 +52,125 @@ macro_rules! check_module {
             }
         }
 
-        $(
-            apply_module!($mod);
-        )*
+        $(apply_module!($mod);)*
     };
 }
 
-macro_rules! check_number_module {
-    ($($mod:ident)*$(=> $($name:ident: |$($param:ident),+| $check:expr)*)?) => {
-        check_module!($($mod)* =>
-            Equals: |A, B| A == B
-            Zero: |T| T == 0
-            Even: |T| T % 2 == 0
-            Odd: |T| T % 2 == 1
-            Less: |A, B| A < B
-            Greater: |A, B| A > B
-            LessOrEqual: |A, B| A <= B
-            GreaterOrEqual: |A, B| A >= B
-            $($(
-            $name: |$($param,)*| $check
-            )*)*
-        );
-    }
+check_module!(u8 u16 u32 u64 u128 usize i8 i16 i32 i64 i128 isize =>
 
-}
+    /// Matches when LHS equals RHS.
+    Equals(
+        passes: 1;1, 5;5
+        fails: 1;10, 3;2
+    ): |A, B| A == B
 
-// check_number_module!(u8 u16 u32 u64 u128 usize);
-// check_number_module!(i8 i16 i32 i64 i128 isize =>
-//     Positive: |T| T > 0
-//     Negative: |T| T < 0
-// );
+    /// Matches when number is zero.
+    Zero(
+        passes: 0
+        fails: 1, 2, 3
+    ): |T| T == 0
+
+    /// Matches when number is even.
+    Even(
+        passes: 2, 4, 6
+        fails: 1, 3, 5
+    ): |T| T % 2 == 0
+
+    /// Matches when number is odd.
+    Odd(
+        passes: 1, 3, 5
+        fails: 2, 4, 6
+    ): |T| T % 2 == 1
+
+    /// Matches when LHS is less than RHS.
+    Less(
+        passes: 1;3, 5;10
+        fails: 5;3, 7;7
+    ): |A, B| A < B
+
+    /// Matches when LHS is greater than RHS.
+    Greater(
+        passes: 3;2, 11;5
+        fails: 5;10, 6;6
+    ): |A, B| A > B
+
+    /// Matches when LHS is less than or equal than RHS.
+    LessOrEqual(
+        passes: 3;5, 9;9
+        fails: 5;2, 9;5
+    ): |A, B| A <= B
+
+    /// Matches when LHS is greater than or equal than RHS.
+    GreaterOrEqual(
+        passes: 9;2, 5;5
+        fails: 2;3, 8;4
+    ): |A, B| A >= B
+
+    /// Matches when number is above 0.
+    Positive(
+        passes: 1, 50, 3
+        fails: 0
+    ): |T| T > 0
+);
+
 
 check_module!(bool =>
-    And: |A, B| A && B
-    Or: |A, B| A || B
-    Xor: |A, B| A ^ B
-    Negate: |T| !T
+
+    /// Matches when both booleans are true.
+    And(
+        passes: true;true
+        fails: true;false, false;true, false;false
+    ): |A, B| A && B
+
+    /// Matches when one boolean is true.
+    Or(
+        passes: true;false, false;true
+        fails: false;false
+    ): |A, B| A || B
+
+    /// Matches exclusive-or booleans.
+    Xor(
+        passes: true;false, false;true
+        fails: true;true, false;false
+    ): |A, B| A ^ B
+
+    /// Matches the negated form of this boolean.
+    Negate(
+        passes: false
+        fails: true
+    ): |T| !T
 );
 
 // Todo: Uppercase and Lowercase when const is supported.
 // Todo: Maybe emoji check as well 👀?
 check_module!(char =>
-    /// So what this is is glitchy
-    Ascii: |C| C.is_ascii()
-    Digit: |C| C.is_ascii_digit()
-    Alphabetic(
-        passes: 'a'; 'b'; 'b'; 'c'
-    ): |C| C.is_ascii_alphabetic()
-    Alphanumeric: |C| C.is_ascii_alphanumeric()
-    Blank: |C| C.is_ascii_whitespace()
-);
+    /// Matches all ascii characters.
+    Ascii(
+        passes: 'a', '1', '&', '~'
+        fails: '¶', '®', 'ℜ', '∑'
+    ): |C| C.is_ascii()
 
-// mod tests {
-//     use super::{Check, Failed, Passed};
-//
-//     struct PositiveOnly<const T: i32>
-//     where
-//         crate::i32::Positive<T>: Passed;
-//
-//     struct AsciiOnly<const C: char>
-//     where
-//         crate::char::Ascii<C>: Passed;
-//
-//     fn positive_test() {
-//         crate::i32::Positive::<5>::passes();
-//         // let doesnt_work = PositiveOnly::<-1>;
-//     }
-//
-//     fn ascii_test() {
-//         let works = AsciiOnly::<'a'>;
-//         // let doesnt_work = AsciiOnly::<'🦀'>;
-//     }
-// }
+    /// Matches digit characters (0-9).
+    Digit(
+        passes: '0', '1', '2'
+        fails: 'a', 'b', 'c'
+    ): |C| C.is_ascii_digit()
+
+    /// Matches all alphabetical characters (a-z).
+    Alphabetic(
+        passes: 'a', 'b', 'c'
+        fails: '1', '2', '3'
+    ): |C| C.is_ascii_alphabetic()
+
+    /// Matches all alphanumeric characters (a-z, 0-9).
+    Alphanumeric(
+        passes: 'a', '1', 'z', '9'
+        fails: '&', '$', '-'
+    ): |C| C.is_ascii_alphanumeric()
+
+    /// Matches blank character (' ').
+    Blank(
+        passes: ' '
+        fails: 'a', '1'
+    ): |C| C.is_ascii_whitespace()
+);
