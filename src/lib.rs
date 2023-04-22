e38a639f06eda8908f4bd4b0a0efe6d0a772b650
deotime <89555032+DeoTimeTheGithubUser@users.noreply.github.com>
2023-04-22 17:23:11 -0500

    ugh documentation

--- src/lib.rs
+++ src/lib.rs
@@ -12,39 +12,34 @@ pub trait Failed {}
 impl<T: Conclusion<true>> Passed for T {}
 impl<T: Conclusion<false>> Failed for T {}
 
+// Theoretically, all custom checks could just be represented as a
+// type alias of `Check`, however currently it complains that it would
+// be an unconstrained bound.
 macro_rules! check_module {
-    ($($mod:ident)* => $($name:ident: |$($param:ident),+$(,)?| $check:expr)*) => {
+    ($($mod:ident)* => $($(#[doc = $doc:expr])?$name:ident$((passes: $($($pass:expr),*);*))?: |$($param:ident),+$(,)?| $check:expr)*) => {
         macro_rules! apply_module {
             ($m:ident) => {
 
                 pub mod $m {
                     $(
-
-                        // Theoretically, all custom checks could just be represented as a
-                        // type alias of `Check`, however currently it complains that it would
-                        // be an unconstrained bound.
+                        $(#[doc = $doc])*
+                        /// ```no_run
+                        #[doc = concat!("use checks::{ Passed, ", stringify!($m), "::* };")]
+                        #[doc = concat!("struct Test<", $(concat!("const ", stringify!($param), ": ", stringify!($m), ", ")),*, ">")]
+                        #[doc = concat!("\twhere ", stringify!($name), "<", stringify!($($param),*), ">: Passed;")]
+                        ///
+                        /// // All compile
+                        $($(
+                            #[doc = concat!("let works = Test::<", stringify!($($pass),*), ">;")]
+                        )*)*
+                        /// ```
                         pub enum $name<$(const $param: $m,)*> {}
 
-                        impl<$(const $param: $m,)*> $name<$($param,)*> {
-                            /// Rust const generic evaluation is strange
-                            ///
-                            /// ----------------------------------
-                            /// Check<{ A && B }> -> Too complex
-                            /// ----------------------------------
-                            ///
-                            /// ----------------------------------
-                            /// const fn apparently_not_complex<const A: bool, const B: bool>() -> bool {
-                            ///     A && B
-                            /// }
-                            ///
-                            /// Check<{ apparently_not_complex::<A, B> } -> Simple
-                            /// ----------------------------------
-                            ///
-                            const fn simplify() -> bool { $check }
+                        impl<$(const $param: $m),*> $name<$($param),*> {
+                            pub const fn simplify() -> bool { $check }
                         }
 
-                        impl<$(const $param: $m,)*> $crate::Conclusion<{ $name::<$($param,)*>::simplify() }> for $name<$($param,)*> {}
-
+                        impl<$(const $param: $m),*> $crate::Conclusion<{ $name::<$($param),*>::simplify() }> for $name<$($param),*> {}
                     )*
                 }
             }
@@ -75,11 +70,11 @@ macro_rules! check_number_module {
 
 }
 
-check_number_module!(u8 u16 u32 u64 u128 usize);
-check_number_module!(i8 i16 i32 i64 i128 isize =>
-    Positive: |T| T > 0
-    Negative: |T| T < 0
-);
+// check_number_module!(u8 u16 u32 u64 u128 usize);
+// check_number_module!(i8 i16 i32 i64 i128 isize =>
+//     Positive: |T| T > 0
+//     Negative: |T| T < 0
+// );
 
 check_module!(bool =>
     And: |A, B| A && B
@@ -91,33 +86,34 @@ check_module!(bool =>
 // Todo: Uppercase and Lowercase when const is supported.
 // Todo: Maybe emoji check as well 👀?
 check_module!(char =>
+    /// So what this is is glitchy
     Ascii: |C| C.is_ascii()
     Digit: |C| C.is_ascii_digit()
-    Alphabetic: |C| C.is_ascii_alphabetic()
+    Alphabetic(
+        passes: 'a'; 'b'; 'b'; 'c'
+    ): |C| C.is_ascii_alphabetic()
     Alphanumeric: |C| C.is_ascii_alphanumeric()
     Blank: |C| C.is_ascii_whitespace()
 );
 
-mod tests {
-    use core::any::Any;
-    use super::{Check, Failed, Passed};
-
-    struct PositiveOnly<const T: i32>
-    where
-        crate::i32::Positive<T>: Passed;
-
-
-    struct AsciiOnly<const C: char>
-        where crate::char::Ascii<C>: Passed;
-
-    fn positive_test() {
-        let works = PositiveOnly::<5>;
-        // let doesnt_work = PositiveOnly::<-1>;
-    }
-
-    fn ascii_test() {
-        let works = AsciiOnly::<'a'>;
-        // let doesnt_work = AsciiOnly::<'🦀'>;
-    }
-
-}
+// mod tests {
+//     use super::{Check, Failed, Passed};
+//
+//     struct PositiveOnly<const T: i32>
+//     where
+//         crate::i32::Positive<T>: Passed;
+//
+//     struct AsciiOnly<const C: char>
+//     where
+//         crate::char::Ascii<C>: Passed;
+//
+//     fn positive_test() {
+//         crate::i32::Positive::<5>::passes();
+//         // let doesnt_work = PositiveOnly::<-1>;
+//     }
+//
+//     fn ascii_test() {
+//         let works = AsciiOnly::<'a'>;
+//         // let doesnt_work = AsciiOnly::<'🦀'>;
+//     }
+// }
