4b2b443e6ba167580ccb914ba375c5036d867020
deotime <89555032+DeoTimeTheGithubUser@users.noreply.github.com>
2023-04-22 23:43:03 -0500

    Add number module

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,7 @@ impl<T: Conclusion<false>> Failed for T {}
 // type alias of `Check`, however currently the compiler complains
 // that it would be an unconstrained bound
 macro_rules! check_module {
-    ($($mod:ident)* => $($(#[doc = $doc:expr])?$name:ident$((passes: $($($pass:tt);*),* fails: $($($fail:expr);*),*))?: |$($param:ident),+| $check:expr)*) => {
+    ($($mod:ident)* => $($(#[doc = $doc:expr])?$name:ident$((passes: $($($pass:literal);*),* fails: $($($fail:literal);*),*))?: |$($param:ident),+| $check:expr)*) => {
         macro_rules! apply_module {
             ($m:ident) => {
 
@@ -57,61 +57,75 @@ macro_rules! check_module {
     };
 }
 
-check_module!(u8 u16 u32 u64 u128 usize i8 i16 i32 i64 i128 isize =>
+macro_rules! check_number_module {
+    ($($mod:ident)*$( => $($extra:tt)*)?) => {
+        check_module!($($mod) * =>
+            /// Matches when LHS equals RHS.
+            Equals(
+                passes: 1;1, 5;5
+                fails: 1;10, 3;2
+            ): |A, B| A == B
 
-    /// Matches when LHS equals RHS.
-    Equals(
-        passes: 1;1, 5;5
-        fails: 1;10, 3;2
-    ): |A, B| A == B
+            /// Matches when number is zero.
+            Zero(
+                passes: 0
+                fails: 1, 2, 3
+            ): |T| T == 0
 
-    /// Matches when number is zero.
-    Zero(
-        passes: 0
-        fails: 1, 2, 3
-    ): |T| T == 0
+            /// Matches when number is even.
+            Even(
+                passes: 2, 4, 6
+                fails: 1, 3, 5
+            ): |T| T % 2 == 0
 
-    /// Matches when number is even.
-    Even(
-        passes: 2, 4, 6
-        fails: 1, 3, 5
-    ): |T| T % 2 == 0
+            /// Matches when number is odd.
+            Odd(
+                passes: 1, 3, 5
+                fails: 2, 4, 6
+            ): |T| T % 2 == 1
 
-    /// Matches when number is odd.
-    Odd(
-        passes: 1, 3, 5
-        fails: 2, 4, 6
-    ): |T| T % 2 == 1
+            /// Matches when LHS is less than RHS.
+            Less(
+                passes: 1;3, 5;10
+                fails: 5;3, 7;7
+            ): |A, B| A < B
 
-    /// Matches when LHS is less than RHS.
-    Less(
-        passes: 1;3, 5;10
-        fails: 5;3, 7;7
-    ): |A, B| A < B
+            /// Matches when LHS is greater than RHS.
+            Greater(
+                passes: 3;2, 11;5
+                fails: 5;10, 6;6
+            ): |A, B| A > B
 
-    /// Matches when LHS is greater than RHS.
-    Greater(
-        passes: 3;2, 11;5
-        fails: 5;10, 6;6
-    ): |A, B| A > B
+            /// Matches when LHS is less than or equal than RHS.
+            LessOrEqual(
+                passes: 3;5, 9;9
+                fails: 5;2, 9;5
+            ): |A, B| A <= B
 
-    /// Matches when LHS is less than or equal than RHS.
-    LessOrEqual(
-        passes: 3;5, 9;9
-        fails: 5;2, 9;5
-    ): |A, B| A <= B
+            /// Matches when LHS is greater than or equal than RHS.
+            GreaterOrEqual(
+                passes: 9;2, 5;5
+                fails: 2;3, 8;4
+            ): |A, B| A >= B
 
-    /// Matches when LHS is greater than or equal than RHS.
-    GreaterOrEqual(
-        passes: 9;2, 5;5
-        fails: 2;3, 8;4
-    ): |A, B| A >= B
+            $($($extra)*)*
+        );
+    }
+}
 
+check_number_module!(u8 u16 u32 u64 u128 usize);
+check_number_module!(i8 i16 i32 i64 i128 isize =>
     /// Matches when number is above 0.
     Positive(
         passes: 1, 50, 3
-        fails: 0
+        fails: 0, -55, -3
     ): |T| T > 0
+
+    /// Matches when number is below 0.
+    Negative(
+        passes: -1, -34, -5
+        fails: 0, 1, 55
+    ): |T| T < 0
 );
 
 check_module!(bool =>
@@ -174,4 +188,3 @@ check_module!(char =>
         fails: 'a', '1'
     ): |C| C.is_ascii_whitespace()
 );
-
